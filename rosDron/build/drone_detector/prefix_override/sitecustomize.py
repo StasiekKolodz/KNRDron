9c561272d2cc97847461b626d7002095b6b2503c
StasiekKolodz <108230658+StasiekKolodz@users.noreply.github.com>
2023-04-18 13:56:12 +0200

    Delete rosDron/build directory

--- rosDron/build/drone_detector/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/stas/Dron/KNRDron/rosDron/install/drone_detector'
--- rosDron/build/drone_interfaces/ament_cmake_python/drone_interfaces/drone_interfaces/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from drone_interfaces.msg._detection import Detection  # noqa: F401
-from drone_interfaces.msg._detections_list import DetectionsList  # noqa: F401
